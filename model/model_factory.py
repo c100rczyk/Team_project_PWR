5ce8dc3a0f0f882b1df7b3811ed1b823fcb97a15
Mateusz Biedka <mateusz.biedka1@o2.pl>
2024-05-14 15:48:47 +0200

    added model evaluation based on  many representatives

---
+++ model/model_factory.py
@@ -0,0 +1,54 @@
+import keras
+import tensorflow as tf
+from distance.DistanceLayer import DistanceLayer
+from distance.EuclideanDistance import EuclideanDistance
+from model.SiameseModel import SiameseModel
+from keras import optimizers
+
+
+class ModelFactory:
+    @staticmethod
+    def create_model(image_size, transfer_model, method, margin):
+        input_layer = keras.layers.Input(image_size + (3,))
+        model = transfer_model(input_layer)
+        model = keras.layers.Flatten()(model)
+        model = keras.layers.Dense(64, activation="relu")(model)
+        model = keras.layers.BatchNormalization()(model)
+        model = keras.layers.Dense(16, activation="relu")(model)
+        model = keras.layers.Lambda(lambda param: tf.math.l2_normalize(param, axis=1))(
+            model
+        )
+        embedding_model = keras.Model(input_layer, model)
+        if method == "triplet_loss":
+            anchor_input = keras.layers.Input(name="anchor", shape=image_size + (3,))
+            positive_input = keras.layers.Input(
+                name="positive", shape=image_size + (3,)
+            )
+            negative_input = keras.layers.Input(
+                name="negative", shape=image_size + (3,)
+            )
+
+            distances = DistanceLayer()(
+                embedding_model(anchor_input),
+                embedding_model(positive_input),
+                embedding_model(negative_input),
+            )
+
+            model = keras.Model(
+                inputs=[anchor_input, positive_input, negative_input], outputs=distances
+            )
+            model = SiameseModel(model, margin=margin)
+
+        elif method == "contrastive_loss":
+            input_1 = keras.layers.Input(image_size + (3,))
+            input_2 = keras.layers.Input(image_size + (3,))
+
+            tower_1 = embedding_model(input_1)
+            tower_2 = embedding_model(input_2)
+
+            merge_layer_distance = keras.layers.Lambda(
+                EuclideanDistance(), output_shape=(1,)
+            )([tower_1, tower_2])
+            model = keras.Model(inputs=[input_1, input_2], outputs=merge_layer_distance)
+        model.compile(optimizer=optimizers.Adam(0.0001), metrics=["accuracy"])
+        return model
